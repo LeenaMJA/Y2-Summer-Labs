f068d85e2589e8597008d4d0e66e6f1e6d0e062a
Leena Abdeen <leenaabdeen1662006@gmail.com>
2022-07-05 10:26:15 +0300

    FrontEndReview - Leena

---
+++ 0.3 FrontEnd Review/main.js
@@ -0,0 +1,13 @@
+function changeBackgroundColor(color) {
+  return background-color: red;
+
+
+<button type="button" id="myBtn" onclick="myFunction()">Change Bg color</button>
+
+function myFunction() {
+  var x = document.getElementById("myBtn");
+  x.disabled = true;
+
+
+///Terminal isn't working 
+
